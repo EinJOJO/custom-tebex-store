7f3b174f8775687dce743cfc9893361218a2ab99
EinJOJO <admin@einjojo.it>
2025-04-13 16:59:27 +0200

    feat: Configure Svelte adapter with custom routes and platform proxy settings

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({
+		routes: {
+			include: ['/*'],
+			exclude: ['<all>']
+		},
+		platformProxy: {
+			configPath: undefined,
+			environment: undefined,
+			persist: undefined
+		}
+	}) }
 };
 
 export default config;
